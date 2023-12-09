91c98072e2034d0753fefdd31e363b66d1b10700
Akshay Gupta <kitallis@gmail.com>
2023-12-09 23:41:48 +0530

    svc can install an app and listen to hooks

--- svc/go.mod
+++ svc/go.mod
@@ -1,15 +1,15 @@
 module buildkansen
 
-go 1.20
+go 1.21.5
 
 require (
-	github.com/gin-contrib/sessions v0.0.5
+	github.com/gin-contrib/sessions v0.0.4
 	github.com/gin-gonic/gin v1.9.0
 	github.com/golang-jwt/jwt/v5 v5.0.0
 	github.com/joho/godotenv v1.5.1
-	golang.org/x/oauth2 v0.7.0
+	golang.org/x/oauth2 v0.15.0
 	gorm.io/driver/sqlite v1.5.0
-	gorm.io/gorm v1.25.0
+	gorm.io/gorm v1.25.5
 )
 
 require (
@@ -20,23 +20,32 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.11.2 // indirect
 	github.com/goccy/go-json v0.10.0 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/gorilla/context v1.1.1 // indirect
+	github.com/gorilla/mux v1.6.2 // indirect
 	github.com/gorilla/securecookie v1.1.1 // indirect
 	github.com/gorilla/sessions v1.2.1 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/markbates/goth v1.78.0 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/mattn/go-sqlite3 v1.14.18 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.9 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.26.0 // indirect
 	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
-	golang.org/x/crypto v0.5.0 // indirect
-	golang.org/x/net v0.9.0 // indirect
-	golang.org/x/sys v0.7.0 // indirect
-	golang.org/x/text v0.9.0 // indirect
-	google.golang.org/protobuf v1.28.1 // indirect
+	golang.org/x/crypto v0.16.0 // indirect
+	golang.org/x/net v0.19.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/appengine v1.6.8 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- svc/main.go
+++ svc/main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
-	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/sessions"
+	"github.com/joho/godotenv"
+	"github.com/markbates/goth"
+	"github.com/markbates/goth/gothic"
+	"github.com/markbates/goth/providers/github"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
 const (
@@ -17,12 +29,89 @@ const (
 )
 
 var (
-	appEnv                 string
-	githubAppId            string
-	githubAppSecret        string
-	githubPrivateKeyBase64 string
-	githubUrl              string
+	appEnv                       string
+	dbName                       string
+	githubAppUrl                 string
+	githubAppId                  string
+	githubClientID               string
+	githubClientSecret           string
+	githubSessionSecret          string
+	githubAuthRedirectUrl        string
+	githubAppRedirectUrl         string
+	githubAppInstallationBaseUrl string
+	githubPrivateKeyBase64       string
+	db                           *gorm.DB
 )
+
+type User struct {
+	Id            string `gorm:"primary_key"`
+	Name          string
+	Email         string    `gorm:"type:varchar(100);unique_index"`
+	CreatedAt     time.Time `gorm:"autoCreateTime"`
+	UpdatedAt     time.Time `gorm:"autoUpdateTime"`
+	DeletedAt     time.Time
+	Installations []Installation `gorm:"foreignKey:UserId"`
+}
+
+type Installation struct {
+	Id               string `gorm:"primary_key"`
+	AccountType      string
+	AccountID        string
+	AccountLogin     string
+	AccountAvatarUrl string
+	UserId           string
+	User             User         `gorm:"foreignKey:UserId;references:Id"`
+	Repositories     []Repository `gorm:"foreignKey:InstallationId"`
+	CreatedAt        time.Time    `gorm:"autoCreateTime"`
+	UpdatedAt        time.Time    `gorm:"autoUpdateTime"`
+	DeletedAt        time.Time
+}
+
+type Repository struct {
+	Id             string `gorm:"primary_key"`
+	Name           string
+	FullName       string
+	Private        bool
+	InstallationId string
+	Installation   Installation `gorm:"foreignKey:InstallationId;references:Id"`
+	CreatedAt      time.Time    `gorm:"autoCreateTime"`
+	UpdatedAt      time.Time    `gorm:"autoUpdateTime"`
+	DeletedAt      time.Time
+}
+
+type GithubInstallationResponseRepository struct {
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	FullName string `json:"full_name"`
+	Private  bool   `json:"private"`
+}
+
+type GithubInstallationResponseAccount struct {
+	Login     string `json:"login"`
+	ID        int    `json:"id"`
+	AvatarURL string `json:"avatar_url"`
+	Type      string `json:"type"`
+}
+
+type GithubInstallationResponseInstallation struct {
+	ID        int                               `json:"id"`
+	Account   GithubInstallationResponseAccount `json:"account"`
+	CreatedAt time.Time                         `json:"created_at"`
+	UpdatedAt time.Time                         `json:"updated_at"`
+}
+
+type GithubInstallationResponseSender struct {
+	Login string `json:"login"`
+	ID    int    `json:"id"`
+	Type  string `json:"type"`
+}
+
+type GithubInstallationResponse struct {
+	Installation GithubInstallationResponseInstallation `json:"installation"`
+	Repositories []GithubInstallationResponseRepository `json:"repositories"`
+	Sender       GithubInstallationResponseSender       `json:"sender"`
+	Action       string                                 `json:"action"`
+}
 
 func initEnv() {
 	e := godotenv.Load()
@@ -32,17 +121,54 @@ func initEnv() {
 	}
 
 	appEnv = os.Getenv("ENV")
+	dbName = os.Getenv("APP_NAME")
+	githubSessionSecret = os.Getenv("GITHUB_SESSION_SECRET")
+	githubAppUrl = os.Getenv("GITHUB_APP_URL")
 	githubAppId = os.Getenv("GITHUB_APP_ID")
-	githubAppSecret = os.Getenv("GITHUB_APP_SECRET")
+	githubClientID = os.Getenv("GITHUB_CLIENT_ID")
+	githubClientSecret = os.Getenv("GITHUB_CLIENT_SECRET")
+	githubAuthRedirectUrl = os.Getenv("GITHUB_AUTH_REDIRECT_URL")
+	githubAppRedirectUrl = os.Getenv("GITHUB_APP_REDIRECT_URL")
 	githubPrivateKeyBase64 = os.Getenv("GITHUB_PRIVATE_KEY_BASE64")
-	githubUrl = os.Getenv("GITHUB_URL")
+	githubAppInstallationBaseUrl = os.Getenv("GITHUB_NEW_INSTALLATION_URL")
+}
+
+func initDB(name string) *gorm.DB {
+	var err error
+	db, err = gorm.Open(sqlite.Open(name), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(&User{})
+	db.AutoMigrate(&Installation{})
+	db.AutoMigrate(&Repository{})
+
+	return db
+}
+
+func initGithubAuth() {
+	gothic.Store = sessions.NewCookieStore([]byte(githubSessionSecret))
+	goth.UseProviders(
+		github.New(githubClientID, githubClientSecret, githubAuthRedirectUrl),
+	)
+}
+
+func tapProviderParam(provider string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		q := c.Request.URL.Query()
+		q.Add("provider", provider)
+		c.Request.URL.RawQuery = q.Encode()
+	}
 }
 
 func initServer() {
 	r := gin.Default()
 	r.GET("/", handleHome)
+	r.GET("/github/auth", tapProviderParam("github"), handleGithubAuth)
+	r.GET("/github/auth/register", tapProviderParam("github"), handleGithubAuthCallback)
+	r.GET("/github/app/register", tapProviderParam("github"), handleGithubAppCallback)
 	r.POST("/github/hook", handleGithubHook)
-  r.GET("/github/register", handleGithubRegistration)
 
 	var err error
 	if appEnv == "production" {
@@ -56,39 +182,158 @@ func initServer() {
 	}
 }
 
-func handleHome(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"message": "you are home"})
-	return
+func handleGithubAuth(c *gin.Context) {
+	q := c.Request.URL.Query()
+	q.Add("provider", "github")
+	c.Request.URL.RawQuery = q.Encode()
+	gothic.BeginAuthHandler(c.Writer, c.Request)
 }
 
-func handleGithubRegistration(c *gin.Context) {
-	body, err := ioutil.ReadAll(c.Request.Body)
+func handleGithubAuthCallback(c *gin.Context) {
+	user, err := gothic.CompleteUserAuth(c.Writer, c.Request)
+
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+
+	u := User{Id: user.UserID, Name: user.Name, Email: user.Email}
+	result := db.Clauses(clause.OnConflict{
+		Columns: []clause.Column{{Name: "id"}},
+		DoUpdates: clause.AssignmentColumns([]string{
+			"email",
+			"name",
+		}),
+	}).Create(&u)
+
+	if result.Error != nil {
+		c.JSON(500, gin.H{"error": "user already exists"})
+		return
+	}
+
+	c.Redirect(http.StatusFound, githubInstallationUrl())
+}
+
+func githubInstallationUrl() string {
+	u := &url.URL{
+		Scheme: "https",
+		Host:   "github.com",
+		Path:   githubAppInstallationBaseUrl,
+	}
+
+	rq := u.Query()
+
+	rq.Set("state", "1")
+	rq.Set("redirect_uri", githubAppRedirectUrl)
+
+	u.RawQuery = rq.Encode()
+
+	return u.String()
+}
+
+func handleGithubAppCallback(c *gin.Context) {
+	body, err := io.ReadAll(c.Request.Body)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to read request body"})
 		return
 	}
 
-	fmt.Println("Received GitHub App callback event:")
+	fmt.Println("Received GitHub App callback:")
 	fmt.Println(string(body))
 
+	queryParams := c.Request.URL.Query()
+	value := queryParams.Get("installation_id")
+	fmt.Println(value)
+
 	c.JSON(http.StatusOK, gin.H{"status": "success"})
+}
+
+func handleHome(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"message": "you are home"})
 }
 
 func handleGithubHook(c *gin.Context) {
-	body, err := ioutil.ReadAll(c.Request.Body)
+	body, err := io.ReadAll(c.Request.Body)
+
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to read request body"})
 		return
 	}
 
-	fmt.Println("Received GitHub App callback event:")
+	var response GithubInstallationResponse
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		fmt.Println("Error:", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to parse request body"})
+		return
+	}
+
+	fmt.Println("Received GitHub App webhook event:")
 	fmt.Println(string(body))
+
+	if response.Action == "created" {
+		err = updateInstallation(response, c)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		}
+	} else {
+		fmt.Printf("Unhandled webhook event for app: %s", response.Action)
+	}
 
 	c.JSON(http.StatusOK, gin.H{"status": "success"})
 }
 
+func updateInstallation(response GithubInstallationResponse, c *gin.Context) error {
+	fmt.Println("Received GitHub App webhook event: created, handling it")
+
+	var user User
+
+	if err := db.First(&user, "id = ?", fmt.Sprint(response.Sender.ID)).Error; err != nil {
+		return errors.New("failed to find a registered user for this installation")
+	}
+
+	tx := db.Begin()
+
+	installation := Installation{
+		Id:               fmt.Sprint(response.Installation.ID),
+		AccountType:      response.Installation.Account.Type,
+		AccountID:        fmt.Sprint(response.Installation.Account.ID),
+		AccountLogin:     response.Installation.Account.Login,
+		AccountAvatarUrl: response.Installation.Account.AvatarURL,
+		UserId:           user.Id,
+	}
+	result := tx.Create(&installation)
+
+	if result.Error != nil {
+		tx.Rollback()
+		return errors.New("failed to save the installation")
+	}
+
+	for _, repo := range response.Repositories {
+		repository := Repository{
+			Id:             fmt.Sprint(repo.ID),
+			Name:           repo.Name,
+			FullName:       repo.FullName,
+			Private:        repo.Private,
+			InstallationId: installation.Id,
+		}
+
+		result := tx.Create(&repository)
+
+		if result.Error != nil {
+			tx.Rollback()
+			return errors.New("failed to save the installation")
+		}
+	}
+
+	tx.Commit()
+
+	return nil
+}
 
 func main() {
 	initEnv()
+	initGithubAuth()
+	initDB(dbName)
 	initServer()
 }
