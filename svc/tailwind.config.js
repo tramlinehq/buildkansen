5d1e8d07e0258aaf0e96af795fff61608002e394
Akshay Gupta <kitallis@gmail.com>
2024-01-22 19:36:58 -0800

    minor ui things

--- svc/tailwind.config.js
+++ svc/tailwind.config.js
@@ -3,12 +3,12 @@ module.exports = {
     content: ["./views/**/*.html"],
     daisyui: {themes: ["dim", "dracula"]},
     theme: {
-        // Some useful comment
         fontFamily: {
             'avenir': ['Avenir Next', 'sans-serif'],
         },
         extend: {
             fontSize: {
+                '2xl': '1.8rem',
                 '11xl': '10rem',
             }
         }
